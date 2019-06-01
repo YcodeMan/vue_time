2a14975a6a3f226996eacf094d63bba40804387e
YcodeMan <1130578809@qq.com>
2019-06-01 12:07:01 +0800

    add movieComing

--- src/components/index/movieComing.vue
+++ src/components/index/movieComing.vue
@@ -1,31 +1,214 @@
 <template>
-    <div class="movieComing">
-        <h2>
-            <a tag="a">
-                <i class="i_tnext"></i>
-                <b>正在热映(43部)</b>
-            </a>
-        </h2>
-        <ul>
-            <a>
-                <div>
-                    <img src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4">
-                    <em><i>7.7</i></em>
-                </div>
-                <p><span>哥斯拉2：怪兽之王</span></p>
-            </a>
-        </ul> 
-    </div>
+  <div class="movieComing">
+    <h2>
+      <a>
+        <i>&gt;</i>
+        <b>正在热映(43部)</b>
+      </a>
+    </h2>
+    <ul>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+      <li>
+        <a>
+          <div>
+            <img
+              src="//imgproxy.mtime.cn/get.ashx?uri=http%3A%2F%2Fimg5.mtime.cn%2Fmt%2F2019%2F05%2F23%2F095633.44711442_1280X720X2.jpg&width=130&height=195&clipType=4"
+            >
+            <em>
+              <i>7.7</i>
+            </em>
+          </div>
+          <p>
+            <span>哥斯拉2：怪兽之王</span>
+          </p>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'movieComing'
-}
+  name: "movieComing"
+};
 </script>
 
-<style lang="sass" scoped>
-    
+<style lang="scss" scoped>
+.movieComing {
+  padding: .12rem .34rem .34rem;
+  h2 {
+    width: 100%;
+    height: .8rem;
+    line-height: .8rem;
+    position: relative;
+    a {
+      height: 100%;
+      i {
+       position: absolute;
+       right: .1rem;
+       width: .4rem;
+       font-size: .4rem;
+       font-weight: 200;
+      }
+      b {
+        display: inline-block;
+        width: 3.4rem;
+        height: .56rem;
+        font-size: .35rem;
+      }
+    }
+  }
+  ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+          width: 1.7rem;
+          height: 3.4rem;
+          div {
+              position: relative;
+              width: 1.45rem;
+              height: 2.28rem;
+              margin: 0 .14rem ;
+              img {
+                 width: 1.5rem;
+              }
+              em {
+                  position: absolute;
+                  top: .06rem;
+                  right: 0;
+                  width: .42rem;
+                  height: .4rem;
+                  text-align: center;
+                  line-height: .4rem;
+                  background-color: #659d0e;
+                  i {
+                      color: #fff
+                  }
+              }
+          }
+          p {
+              width: 1.6rem;
+              height: .88rem;
+              overflow: hidden;
+              padding-top: .1rem;
+              span {
+                  width: 1.5rem;
+                  height: .82rem;
+                  font-size: .32rem
+              }
+          }
+      }
+  }
+}
+
+ul {
+  overflow: hidden;
+}
 </style>
+
 
 

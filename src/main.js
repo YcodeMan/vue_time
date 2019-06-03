03515c0329bffbf9b07f99d10fd3db08751e23ff
YcodeMan <1130578809@qq.com>
2019-06-03 10:47:49 +0800

    add vue-touch

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routers/index'
 import store from './store/index'
-import VueTouch from './vue-touch'
+import VueTouch from 'vue-touch'
 
 // 使用vue-touch
 Vue.use(VueTouch, {name: 'v-touch'})
